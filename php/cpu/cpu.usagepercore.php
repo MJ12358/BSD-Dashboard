dd21d0b3443f2e19e34b097e44abfb6b30a6adcd
MJ12358 <wtpltlf.dev@gmail.com>
2019-08-15 19:53:10 -0500

    Add total usage

--- php/cpu/cpu.usagepercore.php
+++ php/cpu/cpu.usagepercore.php
@@ -2,9 +2,14 @@
 require_once 'cpu.php';
 
 class CpuUsagePerCore extends Cpu {
+
+	public $total_usage;
 	
 	public function __construct() {
-		echo json_encode($this->getUsage());
+		echo json_encode(array(
+			'data' => $this->getUsage(),
+			'total' => $this->total_usage
+		));
 	}
 
 	public function getUsage() {
@@ -12,10 +17,11 @@ class CpuUsagePerCore extends Cpu {
 		$result = explode("\n", Shell::exec($cmd));
 		$results = array();
 		for ($i = 0; $i < count($result); $i++) {
-			$line = preg_split(',', $value);
-			$tmp = str_replace('%', '', prev(end($line)));
-			$results['CPU ' . $i] = 100 - floatval($tmp);
+			$line = explode(',', $result[$i]);
+			$tmp = str_replace('% idle', '', end($line));
+			$results['CPU ' . $i] = round(100 - floatval(trim($tmp)), 2);
 		}
+		$this->total_usage = round(array_sum(array_values($results)), 2);
 		return $results;
 	}
 
