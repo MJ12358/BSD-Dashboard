48e10fcd33760f19581adacc4f61d56ebe5e3f86
MJ12358 <wtpltlf.dev@gmail.com>
2019-08-19 19:06:34 -0500

    Optimize and add totals

--- php/disk/disk.usage.php
+++ php/disk/disk.usage.php
@@ -3,44 +3,39 @@ require_once 'disk.php';
 
 class Usage extends Disk {
 
+	public $total_ds = 0;
+	public $total_snap = 0;
+
 	public function __construct() {
 		echo json_encode(array(
 			'data' => $this->getDatasetUsage(),
-			'total' => $this->getPoolTotal()
+			'total' => $this->getPoolTotal(),
+			'total_ds' => $this->total_ds,
+			'total_snap' => $this->total_snap
 		));
 	}
 
   public function getPoolTotal() {
     $cmd = 'zpool list -o name,size | tail -1';
-    $pool_total = explode(' ', Shell::exec($cmd))[2];
-		// return Convert::to_bytes($pool_total);
-		return $pool_total;
+		$result = explode(' ', Shell::exec($cmd))[2];
+		return Convert::to_bytes($result);
   }
 
   public function getDatasetUsage() {
     $cmd = 'zfs list -o name,usedds,usedsnap | tail -n +3';
     $datasets = explode(PHP_EOL, preg_replace('/\h+/', ' ', Shell::exec($cmd)));
     $result = array();
-    foreach($datasets as $key => $value) {
+    foreach ($datasets as $key => $value) {
       $dataset = preg_split('/\/|\s/', $value);
-      $name = $dataset[1];
-      $used_ds = $dataset[2];
-      $used_snap = $dataset[3];
-      // $result[$name] = array(
-      //   'used_ds' => array(
-      //     'bytes' => Convert::to_bytes($used_ds),
-      //     'formatted' => $used_ds
-      //   ),
-      //   'used_snap' => array(
-      //     'bytes' => Convert::to_bytes($used_snap),
-      //     'formatted' => $used_snap
-      //   )
-			// );
-			$result[$name] = array(
-				'used_ds' => Convert::to_bytes($used_ds),
-				'used_snap' => Convert::to_bytes($used_snap)
+			$result[$dataset[1]] = array(
+				'used_ds' => Convert::to_bytes($dataset[2]),
+				'used_snap' => Convert::to_bytes($dataset[3])
 			);
-    }
+		}
+		foreach ($result as $key => $value) {
+			$this->total_ds += $value['used_ds'];
+			$this->total_snap += $value['used_snap'];
+		}
     return $result;
   }
 
