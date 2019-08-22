d6ca0b9edc435e8622f92a73a5fca7fd7aa6ae72
MJ12358 <wtpltlf.dev@gmail.com>
2019-08-21 19:53:46 -0500

    Optimize and add total bandwidth

--- php/network/network.bandwidth.php
+++ php/network/network.bandwidth.php
@@ -1,9 +1,10 @@
 <?php
+//~ sysctl dev.ix | grep queue (good stuff here)
 require_once 'network.php';
 
 class NetworkBandwidth extends Network {
 
-	private $network_bandwidth_total;
+	private $network_bandwidth_total = 0;
 
 	public function __construct() {
 		echo json_encode(array(
@@ -13,25 +14,33 @@ class NetworkBandwidth extends Network {
 	}
 
   // looping through the interfaces adds one second to rtt for each interface
-  public function getBandwidth(...$interface) {
+  private function getBandwidth() {
     $result = array();
-    // $value = 'all';
-    // foreach(self::$if_count as $key => $value) {
-      $cmd = 'netstat -w 1 -q 1 -b4nW | tail -1';
-      // $cmd = 'netstat -I ' . $value . ' -w 1 -q 1 -b4nW | tail -1';
-      $stats = preg_split('/\s+/', Shell::exec($cmd));
-      $bytes_in = $stats[3];
-			$bytes_out = $stats[6];
-			$this->network_bandwidth_total = $bytes_in + $bytes_out;
-      return array(
-        'bytes_in' => $bytes_in,
+    foreach ($this->getInterfaceNames() as $key => $value) {
+      $cmd = 'netstat -I ' . $value . ' -w 1 -q 1 -b4nW | tail -1';
+      $output = preg_split('/\s+/', Shell::exec($cmd));
+      $bytes_in = $output[3];
+			$bytes_out = $output[6];
+			$result[$value] = array(
+				'bytes_in' => $bytes_in,
 				'bytes_out' => $bytes_out,
-        // 'formatted_in' => Convert::from_bytes($bytes_in),
-        // 'formatted_out' => Convert::from_bytes($bytes_out)
-      );
-    // }
-    // return $result;
-  }
+				'total' => $bytes_in + $bytes_out
+			);
+		}
+		foreach ($result as $key => $value) {
+			$this->network_bandwidth_total += $value['total'];
+		}
+    return $result;
+	}
+	
+//   public static function tx_rx_total() {
+//     $tx_rx = array();
+//     $tx = 'sysctl -n dev.ix.0.queue0.tx_packets';
+//     $tx_rx[] = Shell::exec($tx);
+//     $rx = 'sysctl -n dev.ix.0.queue0.rx_packets';
+//     $tx_rx[] = Shell::exec($rx);
+//     return $tx_rx;
+//   }
 
 }
 
